80be4d6907f690301f966c7123f8dbd8f037950d
Pham Minh Vien <PhamMinhVien@outlook.com>
2026-07-03 14:47:37 +0700

    feat: add warning badge legend

--- icd10/src/components/TreeView.js
+++ icd10/src/components/TreeView.js
@@ -189,22 +189,22 @@ export class TreeView {
     
     let labels = '';
     if (item["MA_KHONG_ĐUOC_DUNG_LA_BENH_CHINH"]) {
-      labels += `<span class="badge badge-red" title="Không được dùng làm bệnh chính">Cấm làm bệnh chính</span>`;
+      labels += `<span class="badge badge-red" title="Mã không được dùng làm bệnh chính">Cấm làm bệnh chính</span>`;
     }
     if (item["MA_KHONG_KHUYEN_KHICH_DUNG_LA_BENH_CHINH"]) {
-      labels += `<span class="badge badge-orange" title="Không khuyến khích dùng làm bệnh chính">K.khuyến khích bệnh chính</span>`;
+      labels += `<span class="badge badge-orange" title="Mã không khuyến khích dùng làm bệnh chính">K.khuyến khích bệnh chính</span>`;
     }
     if (item["MA_KHONG_ĐUOC_SU_DUNG_VI_CO_MA_4_HOAC_5_KY_TU_CU_THE_HON"]) {
-      labels += `<span class="badge badge-red" title="Không được dùng vì có mã chi tiết hơn">Cần mã chi tiết hơn</span>`;
+      labels += `<span class="badge badge-brown" title="Mã không được sử dụng vì có mã 4 hoặc 5 ký tự cụ thể hơn">Cần mã chi tiết hơn</span>`;
     }
     if (item["CHI_SU_DUNG_MA_HOA_NGUYEN_NHAN_TU_VONG"]) {
       labels += `<span class="badge badge-purple" title="Chỉ sử dụng mã hóa nguyên nhân tử vong">Chỉ dùng tử vong</span>`;
     }
     if (item["CAC_MA_BENH_CHI_CÓ_HOAC_CHU_YEU_CO_O_NU_GIOI"] || item["CAC_MA_BENH_CHI_CO_HOAC_CHU_YEU_CO_O_NU_GIOI"]) {
-      labels += `<span class="badge badge-pink" title="Chỉ hoặc chủ yếu ở nữ giới">Nữ giới</span>`;
+      labels += `<span class="badge badge-pink" title="Các mã bệnh chỉ có hoặc chủ yếu có ở nữ giới">Nữ giới</span>`;
     }
     if (item["CAC_MA_BENH_CHI_CO_HOAC_CHU_YEU_CO_O_NAM_GIOI"]) {
-      labels += `<span class="badge badge-blue" title="Chỉ hoặc chủ yếu ở nam giới">Nam giới</span>`;
+      labels += `<span class="badge badge-blue" title="Các mã bệnh chỉ có hoặc chủ yếu có ở nam giới">Nam giới</span>`;
     }
     
     const badgesHtml = labels ? `<div class="icd-badges">${labels}</div>` : '';
